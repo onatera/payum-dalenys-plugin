34aaa1afa0ff8b52aca06db6c61fa98faaf8825b
cboutinon <claude.b@onatera.com>
2023-11-06 11:38:20 +0100

    🔊 [INTL-4928] Add log for order paided cancelled

--- src/Controller/NotifyController.php
+++ src/Controller/NotifyController.php
@@ -2,6 +2,7 @@
 
 namespace Onatera\PayumDalenysPlugin\Controller;
 
+use Onatera\PayumDalenysPlugin\Payum\Api;
 use Sylius\Component\Core\Model\Payment;
 use Payum\Bundle\PayumBundle\Controller\PayumController;
 use Payum\Core\Request\Notify;
@@ -27,11 +28,21 @@ class NotifyController implements ContainerAwareInterface
             return new Response(sprintf('Order not %s found.', $orderId), 400);
         }
 
+        if ($request->query->get('EXECCODE') === Api::EXECCODE_SUCCESSFUL && ($order->getPaymentState() === Payment::STATE_CANCELLED || $order->getState() === Order::STATE_CANCELLED)) {
+            $logger = $this->container->get('logger');
+            $logger->critical('[Dalenys] Order paided and cancelled.', [
+                'orderId' => $orderId,
+                'transactionId' => $request->query->get('TRANSACTIONID'),
+            ]);
+
+            return new Response(sprintf('Order %s paided and cancelled.', $orderId), 400);
+        }
+
         $payments = $this->container->get('sylius.repository.payment')->findBy(['order' => $order], ['createdAt' => 'DESC']);
         if (count($payments) === 0) {
             return new Response(sprintf('Payment for order %s not found.', $orderId), 400);
         }
-        
+
         return new Response('OK', 200);
     }
 }
