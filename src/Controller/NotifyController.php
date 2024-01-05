ba515e6f73d46f679450063e7909c1122177a872
LeVraiPicou <lucas.f@onatera.com>
2024-01-05 17:24:28 +0100

    add chatter to teams channel to log cancel n paid order

--- src/Controller/NotifyController.php
+++ src/Controller/NotifyController.php
@@ -13,14 +13,28 @@ use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Notifier\ChatterInterface;
+use Symfony\Component\Notifier\Chatter;
+use Symfony\Component\Notifier\Bridge\MicrosoftTeams\MicrosoftTeamsTransport;
+use Symfony\Component\Notifier\Message\ChatMessage;
+
+
 
 class NotifyController implements ContainerAwareInterface
 {
     use ContainerAwareTrait;
+    private ChatterInterface $chatter;
+
+    public function __construct(ChatterInterface $chatter)
+    {
+        $this->chatter = $chatter;
+    }
 
     public function __invoke(Request $request)
     {
         $orderId = $request->query->get('ORDERID');
+        $chatMessage = (new ChatMessage('test'))->transport('microsoftteams');
+        $this->chatter->send($chatMessage);
 
         /** @var Order $order */
         $order = $this->container->get('sylius.repository.order')->findOneBy(['number' => $orderId]);
@@ -35,11 +49,17 @@ class NotifyController implements ContainerAwareInterface
                 'transactionId' => $request->query->get('TRANSACTIONID'),
             ]);
 
+            $chatMessage = (new ChatMessage(sprintf('Order %s paided and cancelled.', $orderId)))->transport('microsoftteams');
+            $this->chatter->send($chatMessage);
+
             return new Response(sprintf('Order %s paided and cancelled.', $orderId), 400);
         }
 
         $payments = $this->container->get('sylius.repository.payment')->findBy(['order' => $order], ['createdAt' => 'DESC']);
         if (count($payments) === 0) {
+            $chatMessage = (new ChatMessage(sprintf('Payment for order %s not found.', $orderId)))->transport('microsoftteams');
+            $this->chatter->send($chatMessage);
+
             return new Response(sprintf('Payment for order %s not found.', $orderId), 400);
         }
 
